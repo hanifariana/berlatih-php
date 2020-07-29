095eb9481f87ba9318d05e9d95dd83e05b2241b7
hanifariana <hanifariana@gmail.com>
2020-07-29 10:30:15 +0700

    first

---
+++ palindrome-angka.php
@@ -0,0 +1,17 @@
+<?php
+$revnum=0;
+function palindrome_angka($angka) {
+  // tulis kode di sini
+    while($angka!=0){
+        $revnum = $revnum*10
+    }
+}
+
+// TEST CASES
+echo palindrome_angka(8) // 9
+echo palindrome_angka(10); // 11
+echo palindrome_angka(117); // 121
+echo palindrome_angka(175); // 181
+echo palindrome_angka(1000); // 1001
+
+?>
