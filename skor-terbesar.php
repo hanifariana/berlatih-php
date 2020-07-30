35a32e34c94ef0b39b3c66b0d7aa3fc3cf76b5f3
hanifariana <hanifariana@gmail.com>
2020-07-30 08:42:19 +0700

    skor-terbesar

--- skor-terbesar.php
+++ skor-terbesar.php
@@ -1,6 +1,53 @@
 <?php
 function skor_terbesar($arr){
 //kode di sini
+    $namaL = "";
+    $kelasL = "";
+    $maxL = 0;
+
+    $namaN = "";
+    $kelasN = "";
+    $maxN = 0;
+
+    $namaJ = "";
+    $kelasJ = "";
+    $maxJ = 0;
+
+    for($i=0;$i<count($arr);$i++){
+        if($arr[$i]["kelas"]=="Laravel" && $arr[$i]["nilai"]>$maxL){
+            $namaL = $arr[$i]["nama"];
+            $kelasL = $arr[$i]["kelas"];
+            $maxL = $arr[$i]["nilai"];
+        }
+        else if($arr[$i]["kelas"]=="React Native" && $arr[$i]["nilai"]>$maxN){
+            $namaN = $arr[$i]["nama"];
+            $kelasN = $arr[$i]["kelas"];
+            $maxN = $arr[$i]["nilai"];
+        }
+        else if($arr[$i]["kelas"]=="React JS" && $arr[$i]["nilai"]>$maxJ){
+            $namaJ = $arr[$i]["nama"];
+            $kelasJ = $arr[$i]["kelas"];
+            $maxJ = $arr[$i]["nilai"];
+        }
+    }
+    $arrFinal["Laravel"] = array(
+        "nama" => $namaL,
+        "kelas" => $kelasL,
+        "nilai" => $maxL,
+    );
+    $arrFinal["React Native"] = array(
+        "nama" => $namaN,
+        "kelas" => $kelasN,
+        "nilai" => $maxN,
+    );
+    $arrFinal["React JS"] = array(
+        "nama" => $namaJ,
+        "kelas" => $kelasJ,
+        "nilai" => $maxJ,
+    );
+    echo "<pre>"; 
+    print_r($arrFinal);
+    echo "</pre>";
 }
 
 // TEST CASES
